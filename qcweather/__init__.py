d289447fcf0503015c73aa6ac27e39aa876f89c4
Florent Herbinger <florent.herbinger@outlook.com>
2020-12-10 01:28:06 -0500

    refactored 'assurance' and 'qa' to 'control' and 'qc'

--- qcweather/__init__.py
+++ qcweather/__init__.py
@@ -4,8 +4,8 @@ __version__ = "0.0.1"
 
 # todo: update module names
 from .weather import *
-from .quality_assurance import *
-from .database_qa import *
+from .quality_control import *
+from .database_qc import *
 
 
 
--- qcweather/quality_control.py
+++ qcweather/quality_control.py
@@ -1,27 +1,21 @@
 from qcweather.weather import Weather
 
-def run_quality_assurance(file_path):
+def run_quality_control(file_path):
     # create pandas dataframe with the meteorological variables from weather file
     weather = Weather.get_weather(file_path)
 
-    # run quality assurance on database
-    weather.database_qa()
+    # # run quality assurance on database
+    # weather.database_qc()
 
     # run quality assurance on the magnitudes of the meteorological variables
-    weather.magnitude_qa()
+    weather.magnitude_qc()
 
     # run quality assurance on the difference in magnitude between adjacent data points for each meteorological
     # variables
-    weather.step_qa()
+    weather.step_qc()
 
     # run quality assurance on the time series for each meteorological variable
-    weather.time_qa()
-
-    # write weather errors to file
-    weather.write_errors()
-
-    # write data points to check to file
-    weather.write_to_check()
+    weather.time_qc()
 
 
 

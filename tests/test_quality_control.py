71c16b30da400e49b51afb36790530c504a79eea
Florent Herbinger <florent.herbinger@outlook.com>
2022-02-08 17:24:37 -0400

    Added test_get_month_rad_graphs()

--- tests/test_quality_control.py
+++ tests/test_quality_control.py
@@ -10,7 +10,9 @@ from pathlib import Path
 def epw_path():
     from pathlib import Path
     # CAN-QC - Montreal YUL 716270 - ISD 2015
-    yield Path("datafiles/epw_files/CAN-QC - Montreal YUL 716270 - ISD 2015.epw")
+    # yield Path("datafiles/epw_files/tampered.epw")
+    yield Path("datafiles/epw_files/CAN_QC_Montreal-McTavish.716120_CWEC2016.epw")
+    # yield Path("datafiles/epw_files/CAN-QC - Montreal YUL 716270 - ISD 2015.epw")
 
 @pytest.fixture()
 def config():
@@ -33,3 +35,9 @@ class TestsQualityControl:
         with capsys.disabled():
             from qcweather import run_quality_control
             run_quality_control(epw_path)
+
+    def test_get_month_rad_graphs(self, config, capsys):
+
+        with capsys.disabled():
+            from qcweather import get_month_rad_graphs
+            get_month_rad_graphs()

81c4c16a1451abb1cf1d236109d951cc5da47a9c
Florent Herbinger <florent.herbinger@outlook.com>
2022-06-13 16:26:44 -0300

    Added pytest fixtures for Weather args; added args to tests

--- tests/test_quality_control.py
+++ tests/test_quality_control.py
@@ -1,6 +1,5 @@
 import pytest
 import os
-import shutil
 from pathlib import Path
 from qcweather import Weather
 """
@@ -11,16 +10,32 @@ from qcweather import Weather
 def epw_path():
     from pathlib import Path
     # CAN-QC - Montreal YUL 716270 - ISD 2015
-    yield Path("datafiles/epw_files/tampered.epw")
+    yield "datafiles\\epw_files\\tampered.epw"
+    # yield "datafiles\\epw_files\\CAN_QC_Montreal-McTavish.716120_CWEC2016.epw"
+    # yield "datafiles\\epw_files\\CAN-QC - Montreal YUL 716270 - ISD 2015.epw"
+
+    # yield Path("datafiles/epw_files/tampered.epw")
     # yield Path("datafiles/epw_files/CAN_QC_Montreal-McTavish.716120_CWEC2016.epw")
     # yield Path("datafiles/epw_files/CAN-QC - Montreal YUL 716270 - ISD 2015.epw")
 
 @pytest.fixture()
-def config():
+def drive_letter():
+    yield "E"
+
+@pytest.fixture()
+def ashrae_path():
+    yield "datafiles\\ashrae\\2017DesignConditions_s.xlsx"
+
+@pytest.fixture()
+def output_dir():
+    yield "datafiles"
+
+@pytest.fixture()
+def config(output_dir):
     import shutil
-    if os.path.isdir(Path("datafiles/raw_tbl")):
-        shutil.rmtree(Path("datafiles/raw_tbl"))
-    Path("datafiles/raw_tbl").mkdir()
+    if os.path.isdir(Path(output_dir) / "raw_tbl"):
+        shutil.rmtree(Path(output_dir) / "raw_tbl")
+    (Path(output_dir) / "raw_tbl").mkdir()
 
 class TestsQualityControl:
     """A class that encapsulates the different tests for the functions found in
@@ -31,25 +46,25 @@ class TestsQualityControl:
     ~~~ FUNCTION TESTS ~~~
     """
 
-    def test_run_quality_control(self, config, capsys, epw_path):
+    def test_run_quality_control(self, config, capsys, epw_path, drive_letter, ashrae_path, output_dir):
 
         with capsys.disabled():
-            from qcweather import run_quality_control
-            run_quality_control(epw_path)
+            weather = Weather.get_weather(epw_path, drive_letter, ashrae_path, output_dir)
+            weather.run_quality_control()
 
     def test_get_month_rad_graphs(self, config, capsys):
 
         with capsys.disabled():
             from qcweather import get_month_rad_graphs
-            get_month_rad_graphs()
+            get_month_rad_graphs(drive_letter, ashrae_path, output_dir)
 
-    def test_get_month_rad_extreme_graphs(self, config, capsys):
+    def test_get_month_rad_extreme_graphs(self, config, capsys, drive_letter, ashrae_path, output_dir):
         with capsys.disabled():
             from qcweather import get_month_rad_extreme_graphs
-            get_month_rad_extreme_graphs()
+            get_month_rad_extreme_graphs(drive_letter, ashrae_path, output_dir)
 
     def test_get_graph(self, config, capsys, epw_path):
         with capsys.disabled():
-            weather = Weather.get_weather(epw_path)
+            weather = Weather.get_weather(epw_path, drive_letter, ashrae_path, output_dir)
             weather.get_graph("hourly", "dew_point")
 
